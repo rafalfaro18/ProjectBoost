bcc485cff54c83f435b3ef304fb0698738013de3
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-05-13 17:02:00 -0600

    extract rotation input to own function

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,12 +14,16 @@ public class Movement : MonoBehaviour
     void Update()
     {
         ProcessThrust();
+        ProcessRotation();
     }
 
     void ProcessThrust() {
         if (Input.GetKey(KeyCode.Space)) {
             Debug.Log("Pressed Space - Thrusting");
         }
+    }
+
+    void ProcessRotation() {
         if (Input.GetKey(KeyCode.A)) {
             Debug.Log("Pressed A - Rotate Left");
         }
