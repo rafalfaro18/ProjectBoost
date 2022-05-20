305f83919ad99cdbca135ff436d6678b432d7859
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-05-19 21:53:26 -0600

    freeze rotation when manually controlling rocket

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -39,6 +39,8 @@ public class Movement : MonoBehaviour
 
     void ApplyRotation(float rotationThisFrame)
     {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate.
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // unfreezing rotation so that physics system can take over.
     }
 }
