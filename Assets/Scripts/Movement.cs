41e50afb0d3a4d045115b6e0bb7afab073b00485
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-05-25 23:42:04 -0600

    Refactor code to use PlayOneShot instead.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
     Rigidbody rb;
     AudioSource audioSource;
     
@@ -27,7 +28,7 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); // Relative to the object.
             if(!audioSource.isPlaying){
-                audioSource.Play();
+                audioSource.PlayOneShot(mainEngine);
             }
         } else {
             audioSource.Stop();
