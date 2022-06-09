3f721b17fc5b24d5a5ccf86c096ce657cca9f86a
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-09 15:58:56 -0600

    Oscillate the oscillator

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,9 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+
     
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period; // continually growing over time.
+        const float tau = Mathf.PI * 2; // A full cycle of a sine wave 6.283.
+        float rawSineWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSineWave + 1f) / 2; // recalculated to 0-1 instead of -1 to 1. Comes back to the origin point.
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = offset;
     }
